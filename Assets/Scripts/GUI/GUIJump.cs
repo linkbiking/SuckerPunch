2afc0f986786165142c1a90fb89f66fc30d39620
linkbiking <linkbiking@gmail.com>
2017-08-09 00:21:21 +0700

    jump and box collider
    Sua jump
    Sua box collider cua obstacles

---
+++ Assets/Scripts/GUI/GUIJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIJump : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnPress(bool isPressed)
+    {
+        if (isPressed)
+        {
+            PlayerController.Instance.JumpProcess();
+        }
+        else
+        {
+
+        }
+    }
+
+}
--- Assets/Scripts/ProcessChem.cs
+++ Assets/Scripts/ProcessChem.cs
@@ -19,7 +19,7 @@ public class ProcessChem : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("other.gameObject.layer: " + other.gameObject.layer);
+        //Debug.Log("other.gameObject.layer: " + other.gameObject.layer);
 
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
