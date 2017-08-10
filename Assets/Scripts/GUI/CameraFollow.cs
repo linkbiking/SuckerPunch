a73c8730a0ed1d0fe5df1477f7e3f5ddd3283e36
linkbiking <linkbiking@users.noreply.github.com>
2017-08-10 18:17:34 +0700

    Buil moi nhat
    sda

--- Assets/Scripts/GUI/CameraFollow.cs
+++ Assets/Scripts/GUI/CameraFollow.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
-	public Transform mainChar;
+public class CameraFollow : MonoBehaviour
+{
+    public Transform mainChar;
 
-	private Vector3 offset;
+    private Vector3 offset;
 
-	// Use this for initialization
-	void Start () {
-		offset = this.transform.position - mainChar.transform.position;
+    // Use this for initialization
+    void Start()
+    {
+        offset = this.transform.position - mainChar.transform.position;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(mainChar != null)
-			this.transform.position = Vector3.right*(mainChar.position.x + offset.x) + Vector3.up*(offset.y + mainChar.position.y) + Vector3.forward*offset.z;
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mainChar != null)
+            this.transform.position = Vector3.right * (mainChar.position.x + offset.x) + Vector3.up * (offset.y + mainChar.position.y*0) + Vector3.forward * offset.z;
+    }
 }
